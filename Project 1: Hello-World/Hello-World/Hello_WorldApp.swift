f4fd0c45898687d544932014b6dc1d36687b97fc
Rohejul Islam <islamrohijulr@gmail.com>
2024-09-28 19:08:20 +0600

    Create a hello world swiftUI app by the system

---
+++ Project 1: Hello-World/Hello-World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello-World
+//
+//  Created by Md. Rohejul Islam on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

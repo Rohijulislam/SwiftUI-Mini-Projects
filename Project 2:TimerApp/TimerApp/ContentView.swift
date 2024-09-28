348e2e7a71f37b354329a407f0a33909ce0a4ecb
Rohejul Islam <islamrohijulr@gmail.com>
2024-09-29 03:02:07 +0600

    init commit of project 2 - TimerApp

---
+++ Project 2:TimerApp/TimerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimerApp
+//
+//  Created by Md. Rohejul Islam on 9/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Timer App!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
